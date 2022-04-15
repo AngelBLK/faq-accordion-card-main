d5c7097b0e7fdf5af6bd9b7f72b1065c290f992a
AngelBLK <jaidenze@gmail.com>
2022-04-14 21:35:42 -0500

    [ADD] transform added

--- js/accordion.js
+++ js/accordion.js
@@ -1,24 +1,27 @@
 const button = document.querySelectorAll(".accordion-btn");
 const text = document.querySelectorAll(".accordion-text");
+const image = document.querySelectorAll('.image');
 
 button.forEach((item, index) => {
   item.addEventListener("click", (e) => {
     e.preventDefault();
+    item.classList.toggle('btn-active');
+    image[index].classList.toggle('img-rotate');
 
     if (!text[index].classList.contains("active")) {
-      text[index].classList.add('active');
+      text[index].classList.add("active");
       text[index].style.height = "auto";
       let height = text[index].clientHeight + "px";
-      text[index].style.height = '0px';
+      text[index].style.height = "0px";
       setTimeout(() => {
         text[index].style.height = height;
       }, 0);
     } else {
-      text[index].style.height = '0px';
+      text[index].style.height = "0px";
       text[index].addEventListener(
         "transitionend",
         (e) => {
-          text[index].classList.remove('active');
+          text[index].classList.remove("active");
         },
         {
           once: true,
